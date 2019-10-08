a533a7506844bba1e91520af8455732ee6de1dab
35136 <56117761+35136@users.noreply.github.com>
2019-10-08 19:32:26 +0300

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello world!" << endl;
+
+	cout << "Line 1" << endl;
+
+	cout << "Line 2" << endl;
+
+	cout << endl;
+
+
+	int a = 7;
+
+	cout << a << endl;
+	int b = 6;
+	int c = 5;
+	cout << a + b + c << endl;
+
+
+	int sum = a + b + c;
+	cout << sum << endl;
+
+	int multiplying = a * b * c;
+	cout << multiplying << endl;
+
+
+	float division = float(a) / float(b);
+	cout << division << endl;
+
+	return 0;
+}
